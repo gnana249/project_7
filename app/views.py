339b19ce818be4f5a09fb4bce611dc8f4d351635
prakash <saignana545@gmail.com>
2023-11-23 21:55:00 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sai(request):
+    return render(request,'sai.html')
+
+
+
+def prakash(request):
+    return render(request,'prakash.html')
+
+    
+
